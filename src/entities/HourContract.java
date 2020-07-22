e80fe5586a98f7000931e7829fd8b32cfd316ae0
jotapesales <joaopaulo_jpds@hotmail.com>
2020-07-22 14:42:56 -0300

    Created the entity HourContract.

---
+++ src/entities/HourContract.java
@@ -0,0 +1,22 @@
+package entities;
+
+import java.util.Date;
+
+public class HourContract {
+	private Date date;
+	private Double valuePerHour;
+	private Integer hours;
+	
+	public double totalValue() {
+		return valuePerHour*hours;
+	}
+
+	public HourContract(Double valuePerHour, Integer hours) {
+		super();
+		this.date = new Date();
+		this.valuePerHour = valuePerHour;
+		this.hours = hours;
+	}
+	
+	
+}
